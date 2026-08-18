e08b4dda16a3c2626eb1b7f483f6e6fa8d4da729
zack <isaacpechherrera@gmail.com>
2026-08-17 19:02:41 -0600

    Add initial project structure with main functionality and tests
    - Create .gitignore file
    - Add VSCode settings for Python environment management
    - Implement Calculator class with a sum method
    - Add test for sum method in tests.py
    - Include empty requirements.txt
    - Add compiled Python files to __pycache__

---
+++ src/main.py
@@ -0,0 +1,6 @@
+import os
+
+class Calculator():
+    def sum(self, a : int, b: int) -> int:
+        return 0
+        
---
+++ src/tests.py
@@ -0,0 +1,4 @@
+from main import Calculator
+
+def test_sums_2_nums():
+    assert Calculator().sum(2,4) == 4
